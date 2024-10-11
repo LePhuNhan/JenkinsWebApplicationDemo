3f44fed44196aad7d008eccb9c9064885efac310
LePhuNhan <lephunhan2401@gmail.com>
2024-10-11 07:44:21 +0700

    test

--- JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
+++ JenkinsWebApplicationDemo/Controllers/WeatherForecastController.cs
@@ -6,7 +6,7 @@ namespace JenkinsWebApplicationDemo.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        string abc = "0";
+        string abc = "1";
         private static readonly string[] Summaries = new[]
         {
             "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
